102f252771e4bdeaff673f82e403103bf75380f2
Juan Carlos <carlos.molero.n@gmail.com>
2026-03-24 18:32:05 -0500

    integrar keymap de zed y eliminar archivos innecesarios

--- scripts/set_wallpapers.sh
+++
@@ -1,2 +0,0 @@
-gsettings set org.gnome.desktop.background picture-uri "file:///home/molerocn/Pictures/wallpapers/light.jpg"
-gsettings set org.gnome.desktop.background picture-uri-dark "file:///home/molerocn/Pictures/wallpapers/dark.jpg"
